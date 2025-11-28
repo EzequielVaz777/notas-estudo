791f2affc8b84ad7805ef9bd8f3e140a7440d444
Ezequiel <vaz195428@gmail.com>
2025-11-27 22:03:38 -0300

    exercício js

---
+++ exercício.js
@@ -0,0 +1,24 @@
+
+//pega o exercício no github
+// Peça ao usuário dois números com casas decimais
+//  (números reais), some os dois e mostre o resultado.
+
+// Entrada
+// sempre colocaremos variáveis, dos tipo:
+// const - algo fixo
+// let - não fixo
+// var - erro
+// pedir info para o usuario usando prompt
+// explicar dentro do prompt de que se trata a informação
+
+const numero1 = prompt("Digite o primeiro número com casas decimais:")
+const numero2 = prompt("Digite o segundo número com casas decimais:")
+
+// Processamento
+// normalmente vem um calculo aqui
+// fazer uma adição
+
+// saida
+//mostra o resultado para o usuario
+// usando alert
+alert("A soma dos dois números é: " + numero1 + " + " + numero2 + " = " + (numero1 + numero2))
